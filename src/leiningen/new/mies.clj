16c0b9c6d7902e5abf8d61b34b49c5b7e0f47fcf
David Nolen <david.nolen@gmail.com>
2013-10-27 22:43:54 -0400

    typo

--- src/leiningen/new/mies.clj
+++ src/leiningen/new/mies.clj
@@ -9,4 +9,4 @@
     (->files data
       ["project.clj" (render "project.clj" data)]
       ["src/{{sanitized}}/core.cljs" (render "core.cljs" data)]
-      ["index.html" (render "index.clj" data)])))
+      ["index.html" (render "index.html" data)])))
