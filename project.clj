20c09fc819fb89890af0f6038567668c43ef30f4
David Nolen <david.nolen@gmail.com>
2013-10-27 22:40:31 -0400

    update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject mies/lein-template "0.1.0-SNAPSHOT"
+(defproject mies/lein-template "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git"
+        :url "https://github.com/swannodette/mies"}
   :eval-in-leiningen true)
