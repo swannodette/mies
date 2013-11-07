ef1c7ec6e487de0ea01b38d3ae2d3c23e3e7de97
David Nolen <david.nolen@gmail.com>
2013-11-07 11:47:57 -0500

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mies/lein-template "0.1.3"
+(defproject mies/lein-template "0.1.4"
   :description "A minimal ClojureScript project template"
   :url "http://github.com/swannodette/mies"
   :license {:name "Eclipse Public License"
