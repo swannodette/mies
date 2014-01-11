7a046c5ddc4af59a060cf1e6c1b554006a54edb6
Scott Feeney <scott@oceanbase.org>
2014-01-11 02:06:43 -0500

    Update ClojureScript dep to 2138

--- src/leiningen/new/mies/project.clj
+++ src/leiningen/new/mies/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2127"]]
+                 [org.clojure/clojurescript "0.0-2138"]]
 
   :plugins [[lein-cljsbuild "1.0.1"]]
 
