9d47afa83e78241e016b60f4980b530b26847a7e
Clint Newsom <hcnewsom@gmail.com>
2014-04-04 14:53:28 -0400

    Update version of ClojureScript to 0.0-2173

--- src/leiningen/new/mies/project.clj
+++ src/leiningen/new/mies/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2138"]]
+                 [org.clojure/clojurescript "0.0-2173"]]
 
   :plugins [[lein-cljsbuild "1.0.2"]]
 
