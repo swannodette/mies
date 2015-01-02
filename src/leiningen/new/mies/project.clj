9ea04bb1e04b67d74b5cf858e97c1def16738137
dnolen <dnolen@cognitect.com>
2015-01-02 18:24:19 -0500

    bump dep

--- src/leiningen/new/mies/project.clj
+++ src/leiningen/new/mies/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2629"]]
+                 [org.clojure/clojurescript "0.0-2644"]]
 
   :plugins [[lein-cljsbuild "1.0.4"]]
 
