f82347cc8473d583735e43efbe1914d40218d5bd
David Nolen <david.nolen@gmail.com>
2015-02-14 11:05:44 -0500

    replace standard lein JVM opts

--- src/leiningen/new/mies/project.clj
+++ src/leiningen/new/mies/project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2850"]]
+  :jvm-opts ^:replace ["-Xmx1g" "-server"]
   :node-dependencies [[source-map-support "0.2.8"]]
   :plugins [[lein-npm "0.4.0"]]
   :source-paths ["src" "target/classes"]
