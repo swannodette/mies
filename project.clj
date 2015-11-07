f0a309bae361c8c18b6842e558320b925d53fcac
Maria Geller <maria.geller2@gmail.com>
2015-11-07 17:31:30 +1300

    Update to ClojureScript 1.7.170.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mies/lein-template "0.6.4"
+(defproject mies/lein-template "0.6.5"
   :description "A minimal ClojureScript project template"
   :url "http://github.com/swannodette/mies"
   :license {:name "Eclipse Public License"
