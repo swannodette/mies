31bcda0b3b35869d038f27e88813506c73974713
Maria Geller <maria.geller2@gmail.com>
2016-01-05 06:56:36 +1300

    0.6.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mies/lein-template "0.6.5"
+(defproject mies/lein-template "0.6.6"
   :description "A minimal ClojureScript project template"
   :url "http://github.com/swannodette/mies"
   :license {:name "Eclipse Public License"
