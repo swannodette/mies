afc5319840d270f949147280373d3902b71e9a76
Frank Leon Rose <esorf@users.noreply.github.com>
2016-01-08 16:19:06 -0500

    Remove :classifier "aot"
    AOT causes cljsbuild to drop cljs version to 0.0-3211 https://github.com/emezeske/lein-cljsbuild/issues/431

--- src/leiningen/new/mies/project.clj
+++ src/leiningen/new/mies/project.clj
@@ -2,9 +2,9 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.170" :classifier "aot"
+                 [org.clojure/clojurescript "1.7.170"
                   :exclusion [org.clojure/data.json]]
-                 [org.clojure/data.json "0.2.6" :classifier "aot"]]
+                 [org.clojure/data.json "0.2.6"]]
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
   :plugins [[lein-npm "0.6.1"]]
   :npm {:dependencies [[source-map-support "0.3.2"]]}
