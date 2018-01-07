0a5fb53087292d861bca2f12eff9da09849e5c31
Maria Geller <maria.geller2@gmail.com>
2018-01-07 19:31:24 +0100

    0.6.21
    Update Clojure and ClojureScript versions.
    Add target-path to clean-targets.

--- src/leiningen/new/mies/project.clj
+++ src/leiningen/new/mies/project.clj
@@ -1,11 +1,11 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.671"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.9.946"]]
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
   :plugins [[lein-npm "0.6.2"]]
   :npm {:dependencies [[source-map-support "0.4.0"]]}
   :source-paths ["src" "target/classes"]
-  :clean-targets ["out" "release"]
+  :clean-targets [:target-path "out" "release"]
   :target-path "target")
