29e46aba377ceec697f1be66f03cfff040f6e7f2
Maria Geller <maria.geller2@gmail.com>
2018-07-05 18:08:49 +0200

    Update ClojureScript version to 1.10.339.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mies/lein-template "0.6.21"
+(defproject mies/lein-template "0.6.22"
   :description "A minimal ClojureScript project template"
   :url "http://github.com/swannodette/mies"
   :license {:name "Eclipse Public License"
--- src/leiningen/new/mies/project.clj
+++ src/leiningen/new/mies/project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.9.946"]]
+                 [org.clojure/clojurescript "1.10.339"]]
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
   :plugins [[lein-npm "0.6.2"]]
   :npm {:dependencies [[source-map-support "0.4.0"]]}
